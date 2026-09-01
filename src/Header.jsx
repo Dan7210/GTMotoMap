f621507da45862695335d06042a0ad260ac6cd8b
Daniel Marquez <dmarquez9@gatech.edu>
2026-09-01 18:09:17 -0400

    Added tech tower position and modified header.

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className="header">
             <img src="/GTMotoMap/Icon128.png" alt="LiteTracker Logo" className="logo" />
-            <h3>GT Motorcycle Parking Map - F2025</h3>
+            <h3>GT Motorcycle Parking Map</h3>
         </header>
     );
 }
